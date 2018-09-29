b7d7c68532ecfa4e200669631eedf68a15651fa7
Ivan Gonzalez <ivandario1998@gmail.com>
2018-09-29 11:19:31 -0500

    First

---
+++ fotos/enviar/index.php
@@ -0,0 +1,32 @@
+<?php
+include('conexion.php');
+if (isset($_POST['btn'])) {
+	$bytesfile = file_get_contents($_FILES['txtimg']['tmp_name']);
+	$comentario = $_POST['txtcontent'];
+	$sql = "INSERT img(img_comentario, img_contenido) VALUES (?,?)";
+	$stm = $conexion->prepare($sql);
+	$stm->bind_param('ss', $comentario, $bytesfile);
+	if($stm->execute()){
+		echo "Imagen subida! :)";
+	}
+}
+
+?>
+<form method="POST" action="" enctype="multipart/form-data">
+	<label>Ingresa comentario</label>
+	<input type="text" name="txtcontent">
+	<br> <br>
+	<label>Ingresa imagen</label>
+	<input type="file" name="txtimg">
+	<br><br>	
+	<button type="submit" name="btn">Agregar</button>
+</form>
+
+<button onclick="doSomething()">Ver imagenes guardadas</button>
+
+
+<script>
+    function doSomething(){
+      window.location.href = "../recibir/";
+    }
+</script>
